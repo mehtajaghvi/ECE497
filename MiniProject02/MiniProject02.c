136f5d721e46acf9c4081ccfdada341dd3dd264b
Ruffin White <whitemrj@rose-hulman.edu>
2012-09-24 21:37:18 -0700

    debug fix

--- MiniProject02/MiniProject02.c
+++ MiniProject02/MiniProject02.c
@@ -29,7 +29,7 @@ int main(int argc, char** argv){
 	//variable declarations
 	struct pollfd fdset[1];
 	int nfds = 1;
-	int timeout = 1000;
+	int timeout = 100;
 	int rc;
 	char* buf[MAX_BUF];
 	int gpio1, gpio2;
