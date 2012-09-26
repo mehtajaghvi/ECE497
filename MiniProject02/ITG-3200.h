9e4533420c3dca7b245de485be91e001fc55f9ad
Ruffin White <whitemrj@rose-hulman.edu>
2012-09-26 11:09:00 -0700

    Added some more subfunctions to read or write all regesters

--- MiniProject02/ITG-3200.h
+++ MiniProject02/ITG-3200.h
@@ -15,7 +15,7 @@
 /**
  * Defines
  */
-#define ITG3200_I2C_ADDRESS 0x69 //7-bit address
+#define ITG3200_I2C_ADDRESS 0x69 //7-bit address that Gyro is originally configured by the break-out board
 
 //-----------
 // Registers
@@ -62,7 +62,7 @@ short int GYRO_ZOUT_OFFSET = 0;
 //----------------
 
 //This function is used to initialize the gyroscope. The function returns the -errno if an error accrues.
-int initialize(int i2cbus, int address){
+short int initialize(int i2cbus, int address){
 
 	char filename[20];
 	int file;
@@ -80,7 +80,7 @@ int initialize(int i2cbus, int address){
 }
 
 //This function is used to initialize the gyroscope. The function returns the -errno if an error accrues.
-int zeroGyro(int file){
+short int zeroGyro(int file){
 
 	GYRO_XOUT_OFFSET = readX(file);
 	GYRO_YOUT_OFFSET = readY(file);
@@ -90,10 +90,80 @@ int zeroGyro(int file){
 
 //This function is used to read the WHO_AM_I_REG of the gyroscope.
 //Usage: int gyroID = readWhoAmI();
-int readWhoAmI(int file)
+short int readWhoAmI(int file)
 {
-  int data=0;
+  short int data=0;
   data = i2c_smbus_read_byte_data(file, WHO_AM_I_REG);
+
+  return data;
+}
+
+//This function is used to write the WHO_AM_I_REG of the gyroscope.
+//Usage: data = readWhoAmI(data);
+short int writeWhoAmI(int file, short int data)
+{
+  i2c_smbus_write_byte_data(file,WHO_AM_I_REG,data);
+
+  return data;
+}
+
+
+//This function is used to read the SMPLRT_DIV_REG of the gyroscope.
+short int readSmplrtDiv(int file)
+{
+  short int data=0;
+  data = i2c_smbus_read_byte_data(file, SMPLRT_DIV_REG);
+
+  return data;
+}
+
+//This function is used to write the SMPLRT_DIV_REG of the gyroscope.
+short int writeSmplrtDiv(int file, short int data)
+{
+  i2c_smbus_write_byte_data(file,SMPLRT_DIV_REG,data);
+
+  return data;
+}
+
+//This function is used to read the DLPF_FS_REG of the gyroscope.
+short int readDlpfFs(int file)
+{
+  short int data=0;
+  data = i2c_smbus_read_byte_data(file, DLPF_FS_REG);
+
+  return data;
+}
+
+//This function is used to write the DLPF_FS_REG of the gyroscope.
+short int writeDlpfFs(int file, short int data)
+{
+  i2c_smbus_write_byte_data(file,DLPF_FS_REG,data);
+
+  return data;
+}
+
+//This function is used to read the INT_CFG_REG of the gyroscope.
+short int readIntCfg(int file)
+{
+  short int data=0;
+  data = i2c_smbus_read_byte_data(file, INT_CFG_REG);
+
+  return data;
+}
+
+//This function is used to write the INT_CFG_REG of the gyroscope.
+short int writeIntCfg(int file, short int data)
+{
+  i2c_smbus_write_byte_data(file,INT_CFG_REG,data);
+
+  return data;
+}
+
+//This function is used to read the INT_STATUS of the gyroscope.
+short int readIntStatus(int file)
+{
+  short int data=0;
+  data = i2c_smbus_read_byte_data(file, INT_STATUS);
 
   return data;
 }
@@ -101,9 +171,9 @@ int readWhoAmI(int file)
 
 //This function is used to read the temperature of the gyroscope.
 //Usage: int gyroTemp = readTemp();
-int readTemp(int file)
+short int readTemp(int file)
 {
-  int data=0;
+  short int data=0;
   data = i2c_smbus_read_byte_data(file, TEMP_OUT_H_REG)<<8;
   data |= i2c_smbus_read_byte_data(file, TEMP_OUT_L_REG);
 
@@ -113,9 +183,9 @@ int readTemp(int file)
 //This function is used to read the X-Axis rate of the gyroscope. The function returns the ADC value from the Gyroscope
 //NOTE: This value is NOT in degrees per second.
 //Usage: int xRate = readX();
-int readX(int file)
+short int readX(int file)
 {
-  int data=0;
+  short int data=0;
   data = i2c_smbus_read_byte_data(file, GYRO_XOUT_H_REG)<<8;
   data |= i2c_smbus_read_byte_data(file, GYRO_XOUT_L_REG);
 
@@ -125,9 +195,9 @@ int readX(int file)
 //This function is used to read the Y-Axis rate of the gyroscope. The function returns the ADC value from the Gyroscope
 //NOTE: This value is NOT in degrees per second.
 //Usage: int yRate = readY();
-int readY(int file)
+short int readY(int file)
 {
-  int data=0;
+  short int data=0;
   data = i2c_smbus_read_byte_data(file, GYRO_YOUT_H_REG)<<8;
   data |= i2c_smbus_read_byte_data(file, GYRO_YOUT_L_REG);
 
@@ -137,11 +207,28 @@ int readY(int file)
 //This function is used to read the Z-Axis rate of the gyroscope. The function returns the ADC value from the Gyroscope
 //NOTE: This value is NOT in degrees per second.
 //Usage: int zRate = readZ();
-int readZ(int file)
+short int readZ(int file)
 {
-  int data=0;
+  short int data=0;
   data = i2c_smbus_read_byte_data(file, GYRO_ZOUT_H_REG)<<8;
   data |= i2c_smbus_read_byte_data(file, GYRO_ZOUT_L_REG);
 
   return data - GYRO_ZOUT_OFFSET;
+}
+
+//This function is used to read the PWR_MGM_REG of the gyroscope.
+short int readPwrMgm(int file)
+{
+  short int data=0;
+  data = i2c_smbus_read_byte_data(file, PWR_MGM_REG);
+
+  return data;
+}
+
+//This function is used to write the PWR_MGM_REG of the gyroscope.
+short int writePwrMgm(int file, short int data)
+{
+  i2c_smbus_write_byte_data(file,PWR_MGM_REG,data);
+
+  return data;
 }
